3f51f3ad32aac21cc3fd66a08946384b06e35347
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-08-27 07:43:20 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,97 @@
-
+const perguntas = [
+  {
+    enunciado: "Vocẽ torce pro coxa ou pro atlhetico?",
+    alternativas: [
+      {
+        texto: "afirmativas 1" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+       {
+        texto: "afirmativas 2" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+    ]
+  },
+  {
+    enunciado: "Você prefere sertanejo ou funk?",
+    alternativas: [
+      {
+        texto: "afirmativas 1" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+       {
+        texto: "afirmativas 2" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+    ]
+  },
+  {
+    enunciado: "",
+    alternativas: [
+      {
+        texto: "afirmativas 1" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+       {
+        texto: "afirmativas 2" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      {
+        texto: "afirmativas 1" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+       {
+        texto: "afirmativas 2" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+    ]
+  },
+  {
+    enunciado: "Pergunta 1",
+    alternativas: [
+      {
+        texto: "afirmativas 1" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+       {
+        texto: "afirmativas 2" ,
+        afirmacao: [
+          "afirmação 1" ,
+          "afirmação 2"
+        ],
+      },
+    ]
+  },
+]
