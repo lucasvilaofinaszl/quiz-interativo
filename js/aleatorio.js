9c6b340d07d7602bbaa0c62294bab11f0dd9339d
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-09-04 12:05:11 -0300

    .

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Jucas", "Emerson","Lucas","Antony","Victor","Wiliam"];
 
+function aleatorio(lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+const nome = aleatorio(nomes)
