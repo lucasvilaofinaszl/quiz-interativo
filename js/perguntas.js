8c9e602f34a0f507e4c927746b28f5d436af79c6
lucasvilaofinaszl <lucas.alves.portela@escola.pr.gov.br>
2025-09-16 11:15:04 -0300

    Update perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,103 +1,103 @@
 const perguntas = [
   {
-    enunciado: "",
-    alternativas: [
+    enunciado: "Qual tipo de comida você prefere?",
+    alternativas: [ comida salgada ou comida doce 
       {
-        texto: "afirmativas" ,
-        afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+        texto: "comida salgada" ,
+        afirmacao: [ 
+          "Você tem muitas opções de pratos deliciosos, como pizzas, pastéis e salgadinhos." ,
+          "Comida salgada é perfeita para refeições mais pesadas e saborosas."
         ],
-        proxima: 
+        proxima 1,
       },
        {
-        texto: "afirmativas 2" ,
+        texto: "comida doce" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "As sobremesas são irresistíveis, com bolos, doces e chocolates!" ,
+          "Doce é uma escolha perfeita para quem adora sabores mais leves e delicados"
         ],
          proxima 1,
       },
     ]
   },
   {
-    enunciado: "Você comida salgada ou doce?",
+    enunciado: "Você prefere uma refeição mais saudável ou mais indulgente?",
     alternativas: [
       {
-        texto: "salgada" ,
+        texto: "Refeição saudável" ,
         afirmacao: [
-          "tem muitas opções de comidas boas" ,
-          ""
+          "Você se preocupa com a saúde, escolhendo alimentos nutritivos e equilibrados" ,
+          "Você gosta de saladas frescas, vegetais e pratos ricos em proteínas e fibras"
         ],
         proxima 2;
       },
        {
-        texto: "sertanejo" ,
+        texto: "Refeição indulgente" ,
         afirmacao: [
-          "" ,
-          "um estilo bom de musica"
+          "Você gosta de se permitir comer algo mais calórico de vez em quando" ,
+          "Frituras, hambúrgueres e doces são suas escolhas preferidas."
         ],
          proxima 2;
       },
     ]
   },
   {
-    enunciado: "",
+    enunciado: "Como você prefere passar o seu tempo livre?",
     alternativas: [
       {
-        texto: "afirmativas 1" ,
+        texto: "Atividades ao ar livre" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você adora atividades como caminhadas, trilhas ou praticar esportes ao ar livre" ,
+          "Estar em contato com a natureza é uma das melhores formas de relaxar e se energizar."
         ],
         proxima 3;
       },
        {
-        texto: "afirmativas 2" ,
+        texto: "Atividades dentro de casa" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você prefere passar o tempo em um ambiente mais tranquilo, assistindo filmes ou lendo livros." ,
+          "ogar videogame, cozinhar ou fazer atividades criativas em casa também são maneiras perfeitas de relaxar."
         ],
          proxima 3;
       },
     ]
   },
   {
-    enunciado: "Pergunta 1",
+    enunciado: "Você curte música? Qual estilo mais te agrada?",
     alternativas: [
       {
-        texto: "afirmativas 1" ,
+        texto: "Música clássica" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você aprecia a serenidade e a complexidade das composições clássicas de grandes mestres." ,
+          "Ouvir música clássica é uma ótima maneira de relaxar e escapar do estresse do dia a dia."
         ],
         proxima 3;
       },
        {
-        texto: "afirmativas 2" ,
+        texto: "Música pop ou sertanjeo" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você ama os ritmos animados e dançantes, seja do pop, sertanejo ou outros estilos modernos." ,
+          "A música pop ou sertanejo sempre te coloca no clima certo para sair e se divertir."
         ],
          proxima 4;
       },
     ]
   },
   {
-    enunciado: "Pergunta 1",
+    enunciado: "Em qual ambiente você se sente mais confortável?",
     alternativas: [
       {
-        texto: "afirmativas 1" ,
+        texto: "Ambiente tranquilo e organizado" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você se sente em paz em espaços calmos, limpos e bem arrumados." ,
+          "Um ambiente tranquilo ajuda você a se concentrar, relaxar e encontrar serenidade."
         ],
       },
        {
-        texto: "afirmativas 2" ,
+        texto: "Ambiente descontraído e animado" ,
         afirmacao: [
-          "afirmação 1" ,
-          "afirmação 2"
+          "Você adora ambientes mais energéticos, como festas e reuniões com amigos." ,
+          "Ambientes descontraídos, com boa música e gente animada, são onde você se sente mais à vontade."
         ],
       },
     ]
